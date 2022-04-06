cb40b7729d8625de6fe28473066d47e39eafa497
thakareshubh <shubhamthakare032@gmail.com>
2022-04-06 10:54:51 +0530

    Check Sqrt until fullfile the condition

---
+++ SqrtOfNegativeNumber/SqrtOfNegativeNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SqrtOfNegativeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter Number");
+            Sqrt();
+            
+        }
+        /// <summary>
+        /// SQRTs this instance.
+        /// </summary>
+        static void Sqrt()
+        {
+            //initialize variabale
+            double c=Convert.ToDouble(Console.ReadLine());
+            double t = c;
+            double epsilon = 1E-15;
+            double temp = t * epsilon;
+            double temp1=Math.Abs(t-c/t);
+            while(temp1>temp)
+            {
+                t = ((c / t) + t) / 2;
+            }
+
+            Console.WriteLine(t);     
+        }
+
+    }
+   
+}
